34912d502634fadab3d538a22ede2b346d4d32f0
Dexshine <b_o_y_s@hotmail.com>
2020-01-17 15:59:19 +0700

    ver1

--- lab6_3.cpp
+++ lab6_3.cpp
@@ -2,7 +2,15 @@
 using namespace std;
 
 char before(char x){
-	//Write your function definition here
+	if(x==65){
+		return 90;
+	}else if(x>90){
+		return 48;
+	}else if(x<65){
+		return 48;
+	}else{
+		return x-1;
+	}
 }
 
 int main(){
